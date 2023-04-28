be9946236251600922eefd3f66da25fe8c088b13
xMurzik <murzik6667@gmail.com>
2023-04-28 10:54:14 +0300

    mocks added

---
+++ src/mocks/mock.js
@@ -0,0 +1,75 @@
+const FLIGHT_POINTS = [
+  'Amsterdam',
+  'Chamonix',
+  'Geneva',
+  'New York',
+  'Moskow',
+  'Tokyo',
+];
+
+const MOVING_ELEMENTS = [
+  'Taxi',
+  'Bus',
+  'Train',
+  'Ship',
+  'Drive',
+  'Flight',
+  'Check-in',
+  'Sightseeing',
+  'Restaurant',
+];
+
+const getRandomElem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const mapCitys = new Map();
+const mapOffers = new Map();
+
+FLIGHT_POINTS.forEach((elem) => {
+  mapCitys.set(elem, {
+    cityName: elem,
+    description: `${elem}, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: '${elem}'`,
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${Math.floor(
+          Math.random() * 10000
+        )}`,
+        description: `${elem} parliament building`,
+      },
+    ],
+  });
+});
+
+MOVING_ELEMENTS.forEach((elem) => {
+  mapOffers.set(elem, {
+    type: elem,
+    offers: Array.from({ length: Math.floor(Math.random() * 10) }, () => ({
+      title: 'Upgrsdfsdfss class',
+      price: Math.floor(Math.random() * 10000),
+    })),
+  });
+});
+
+export const generateObj = (count) => {
+  if (count > 20 || count < 0) {
+    count = 5;
+  }
+  let i = 0;
+  let typeOfMoving;
+  const data = [];
+  while (i <= count) {
+    i++;
+    typeOfMoving = getRandomElem(MOVING_ELEMENTS);
+    data.push({
+      basePrice: Math.floor(Math.random() * 100000),
+      dateFrom: '2019-07-10T22:55:56.845Z',
+      dateTo: '2019-07-11T11:22:13.375Z',
+      destination: mapCitys.get(getRandomElem(FLIGHT_POINTS)),
+      isFavorite: [true, false][Math.floor(Math.random() * 2)],
+      offers: mapOffers.get(typeOfMoving),
+      type: typeOfMoving,
+    });
+  }
+
+  return data;
+};
