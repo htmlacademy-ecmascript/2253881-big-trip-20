ffc53d0b225c9ec6ec9cc7e74bb0d9541091bbd1
xMurzik <murzik6667@gmail.com>
2023-05-01 18:13:38 +0300

    added no events component and fixes for sort list

--- src/main.js
+++ src/main.js
@@ -4,17 +4,29 @@ import TripInfo from './view/TripInfo';
 import EventList from './view/EventList';
 import { render, RenderPosition } from './framework/render';
 import UlListRender from './view/UlListRender';
+import ErrorDwnl from './view/ErrorDwnl';
+import { generateObj } from './mocks/mock';
+
+const infosContent = generateObj(Math.floor(Math.random() * 10));
+// const infosContent = [];
 
 const filterContainerElem = document.querySelector('.trip-controls__filters');
 const sortContainerElem = document.querySelector('.trip-events');
 const tripMainContElem = document.querySelector('.trip-main');
 
-render(new TripInfo(), tripMainContElem, RenderPosition.AFTERBEGIN);
-render(new ListOfFilters(), filterContainerElem, RenderPosition.BEFOREEND);
-render(new ListOfSort(), sortContainerElem, RenderPosition.AFTERBEGIN);
+if (infosContent.length !== 0) {
+  render(new TripInfo(), tripMainContElem, RenderPosition.AFTERBEGIN);
+  render(new ListOfSort(), sortContainerElem, RenderPosition.AFTERBEGIN);
 
-//общий список ul
-render(new EventList(), sortContainerElem, RenderPosition.BEFOREEND);
-
-const contentList = new UlListRender();
-contentList.init();
+  //общий список ul
+  render(new EventList(), sortContainerElem, RenderPosition.BEFOREEND);
+  const contentList = new UlListRender(infosContent);
+  contentList.init();
+} else {
+  render(new ErrorDwnl(), sortContainerElem, RenderPosition.BEFOREEND);
+}
+render(
+  new ListOfFilters(infosContent),
+  filterContainerElem,
+  RenderPosition.BEFOREEND
+);
